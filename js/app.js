6bf125c522b9a19dad1469ea37147b2959a816a6
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-13 04:06:27 +0600

    added average ratings and total ratings count

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <h5>Average Rating: ${product.rating.rate} &#11088 | Total Ratings ${product.rating.count} </h5>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
